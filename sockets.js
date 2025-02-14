f2db8c6be0222c44983bc47ec7fb518e2cc2c91f
Bob-Karemera-Shema <72112797+Bob-Karemera-Shema@users.noreply.github.com>
2025-02-14 14:03:44 +0000

    final commit

---
+++ sockets.js
@@ -0,0 +1,38 @@
+let playerCount = 0;
+
+function listen(io) {
+    const pongNamespace = io.of('/pong');
+    
+    pongNamespace.on('connection', (socket) => {
+        let room;
+
+        console.log('User connected: ', socket.id);
+
+        socket.on('ready', () => {
+            room = 'room' + Math.floor(playerCount / 2);
+            socket.join(room);
+
+            console.log('Player ready: ', socket.id);
+            playerCount++;
+
+            if (playerCount % 2 === 0) {
+                pongNamespace.in(room).emit('startGame', socket.id);
+            }
+        });
+
+        socket.on('paddleMove', (paddleData) => {
+            socket.to(room).emit('paddleMove', paddleData);
+        });
+
+        socket.on('ballMove', (ballData) => {
+            socket.to(room).emit('ballMove', ballData);
+        });
+
+        socket.on('disconnect', (reason) => {
+            console.log(`Client ${socket.id} disconnected: ${reason}`);
+            socket.leave(room);
+        });
+    });
+}
+
+module.exports = { listen };
